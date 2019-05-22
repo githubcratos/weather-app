1e0dd8a3f7f76f110e0d8b18b493fd4060c9b987
githubcratos <edwardcampbell2014@gmail.com>
2019-05-22 13:37:54 +0100

    This is my new weather app

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+console.log('Client side javascript file is loaded!')
+
+
+
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const weather= document.querySelector('#weather')
+const error = document.querySelector('#error')
+const loc = document.querySelector('#loc')
+
+//weather.textContent = "Nigga"
+
+weatherform.addEventListener('submit', (e) =>{
+e.preventDefault()
+const location = search.value
+weather.textContent = "Loading"
+loc.textContent = ""
+
+
+fetch('http://localhost:3001/weather?address='+ location ).then((response) =>{
+     response.json().then((data) => {
+         if(data.error){
+             console.log(data.error)
+             weather.textContent = ""
+             error.textContent = data.error
+         }else{
+
+            console.log(data.location)
+            console.log(data.forecast)
+            loc.textContent = data.location
+            weather.textContent = data.forecast
+         }
+        
+     })
+
+})
+console.log(location)
+})
